aa96ed508a6fdff675640d26914639f912cb08fc
Blaine F Garrett <blaine@blainegarrett.com>
2019-04-14 11:07:28 -0500

    Working version of doing local dependency dev.

--- env-config.js
+++
@@ -1,5 +0,0 @@
-const prod = process.env.NODE_ENV === 'production';
-
-// This is needed for docs to compile with the gh-pages route prefix
-// See: https://github.com/zeit/next.js/issues/3335
-module.exports = {'process.env.BACKEND_URL': prod? '/armature' : '' };
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import PageBase from '../docs/src/layout/PageBase';
-import { withStyles } from '@material-ui/core/styles';
-import withRoot from '../src/withRoot';
-import withRedux from '../src/withRedux'
-import {initStore} from '../src/store';
+import React from "react";
+//import PageBase from "../docs/src/layout/PageBase";
+//import { withStyles } from "@material-ui/core/styles";
+//import withRoot from "../src/withRoot";
+//import withRedux from "../src/withRedux";
+//import { initStore } from "../src/store";
 
-class Index extends React.Component {
+export default class Index extends React.Component {
   render() {
     return (
       <PageBase>
-        <p style={{ color: 'blue' }}>
-          hi there
-        </p>
+        <p style={{ color: "blue" }}>hi there</p>
       </PageBase>
     );
   }
 }
 //export default withRoot(withStyles({})(Index));
-export default withRedux(initStore, null, {})(withRoot(withStyles({})(Index)));
+//export default withRedux(initStore, null, {})(withRoot(withStyles({})(Index)));
